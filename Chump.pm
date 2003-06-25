426f2b8e615aaefebf59d3bd9c55e415cfbc27a3
Simon Wistow <simonw@cpan.org>
2003-06-25 16:34:20 +0000

    Import of SIMONW/Template-Plugin-Chump-1.1 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-Chump
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         SIMONW/Template-Plugin-Chump-1.1.tar.gz
    gitpan-cpan-author:       SIMONW
    gitpan-cpan-maturity:     released

--- Chump.pm
+++ Chump.pm
@@ -6,7 +6,7 @@ use Template::Plugin::Filter;
 use base qw(Template::Plugin::Filter);
 use vars qw($VERSION $FILTER_NAME);
 
-$VERSION = '1.0';
+$VERSION = '1.1';
 $FILTER_NAME = 'chump';
 
 
@@ -31,8 +31,16 @@ Template::Plugin::Chump - provides a Template Toolkit filter for Chump like synt
 	This will turn the word 'foo' into a link to bar.com
 	[foo|http://bar.com]
 
-	This will get turned into an inline image with the label 'foo'
+	This will get turned into an image link 
+    +[http://foo.com/quux.jpg]
+
+	This will get turned into an inline image with the label 'bar'
 	+[bar|http://foo.com/quux.jpg]
+
+    This will get turned into an image with the label 'bar' which 
+    is a link to http://foobar.com
+    +[bar|http://foobar.com|http://url.of.image.com/image.jpg]
+
 
 	[% END %]
 
@@ -43,7 +51,8 @@ Template::Plugin::Chump - provides a Template Toolkit filter for Chump like synt
 	[% FILTER chump %]
 	=[foo|regeeeeexp]
 	[% END %]
-	the subroutine subref will now be called.
+	the subroutine subref will now be called for all links 
+    of the format =[<stuff>]
 
 	[% FILTER chump( {links=>0}) %]
 	links won't be parsed so this ...
@@ -54,11 +63,28 @@ Template::Plugin::Chump - provides a Template Toolkit filter for Chump like synt
 	</body>
 	</html>
 
+Alternatively you can pass in a Text::Chump object and use that.
+
+
+	[% USE Chump({ chump => my_chump_object }) %]
+
 
 =head1 DESCRIPTION
 
 Chump is a simplified markup language that allows for simple markup of 
 text to include urls, links and inline images.
+
+Chump is based on an original idea by Bijan Parsia who wrote a bot named DiaWebLogBot in Squeak
+for the Monkeyfist Daily Churn. Subsequently Useful Inc. "stole all his good ideas" for their own 
+IRC bot.
+
+The bot is available from http://usefulinc.com/chump/ and the original page that uses this form of 
+markup is http://pants.heddley.com 
+
+From there the syntax was adopted by various other projects where more 
+complex solutions such as Text::WikiFormat or HTML::FromText aren't
+needed or don't have the right features and an extensible parser was
+written because that's what good little programmers do.
 
 B<Text::Chump> is a surprisingly (too the author if not anyone else), 
 flexible and powerful Chump parser. From humble beginnings it has evolved 
@@ -86,6 +112,7 @@ Just pass them in either when loading the plugin or filtering with  it.
 
 Like that.
 
+
 You can also call the new_type and install methods from Text::Chump
 using
 
@@ -101,7 +128,7 @@ automatically you need something which returns a sub ref.
 
 So, in your CGI script you need something like
 
-	$vars->{sub} = sub { return sub { return uc $_[1] } };
+	$vars->{uc} = sub { return sub { return uc $_[1] } };
 
 	# stuff
 
@@ -109,7 +136,7 @@ So, in your CGI script you need something like
 
 And then in your template you do
 
-	[% Chump.install('equal', '=', sub) %]
+	[% Chump.install('equal', '=', uc) %]
 	[% FILTER chump %]
 		=[foo]
 		This will give FOO
@@ -117,6 +144,29 @@ And then in your template you do
 
 
 Que convenient :)
+
+Alternatively you can pass in a fully formed B<Text::Chump>
+object as a template var and use that instead inorder to more 
+cleanly seperate code from presentation.
+
+	sub uc { return uc $_[1] };
+
+	my $tc = Text::Chump->new();
+	$tc->install('link',\&uc);
+	$tc->new_type('equal','=',\&uc);
+	
+	$tt->process($template, { my_chump => $tc });
+
+
+And then in the template
+
+	[% USE Chump ( { chump => my_chump } ) %]
+
+	[% FILTER chump %]
+		=[foo]
+		This will give FOO
+	[% END %]
+
 
 
 =head1 AUTHOR
@@ -148,10 +198,18 @@ sub init {
 	my ($self,@args)  = @_;
 	my $config = (ref $args[-1] eq 'HASH')? pop @args : {};
 
+	my $tc;
+
+	if (defined $config->{chump}) {
+		$tc = $config->{chump};
+	} else {
+	    $tc = Text::Chump->new($config);
+    }    
+
 	
 
 	$self->{_DYNAMIC} = 1;
-	$self->{_TC}      = Text::Chump->new($config);
+	$self->{_TC}      = $tc;
 
 
 	$self->install_filter($FILTER_NAME);
